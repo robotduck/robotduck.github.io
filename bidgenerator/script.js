e8ff8d57526a43c19cb502029b8e30905db9e236
robotduck <duck@robotduck.com>
2023-10-04 21:14:51 +0100

    added typing effect

--- bidgenerator/script.js
+++ bidgenerator/script.js
@@ -2,13 +2,12 @@ window.addEventListener('load', function () {
 
 	var generateButton = document.getElementById('generatebutton');
 
-	console.log(phrases[1]);
-
 	generateButton.onclick = function() {
-		document.getElementById('resultfield').innerText = GenerateResult();
+		ShowResult();
 	};
 
 })
+
 
 function GenerateResult()
 {
@@ -25,3 +24,36 @@ function GetRandomElement(array)
 {
 	return array[Math.floor(Math.random()*array.length)];
 }
+
+
+function ShowResult() {
+
+	var result = GenerateResult();
+	var resultField = document.getElementById('resultfield');
+
+	var speed = 35; 
+	
+	resultField.innerHTML = "...";
+	
+	setTimeout(function(){
+		resultField.innerHTML = "";
+		TypeResult(resultField, result, speed)
+	}, 1000);
+	
+}
+
+function TypeResult(resultField, result, speed)
+{
+	var i = resultField.innerHTML.length;
+	console.log(result,speed,i);
+
+	if (i < result.length) {
+		i++;
+		resultField.innerHTML = result.substring(0,i);
+		setTimeout(function(){
+			TypeResult(resultField, result, speed)
+		}, speed);
+	}
+}
+
+
